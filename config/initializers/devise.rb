ce458100018ef1d340578387e7e0656d3ad0153f
Paulo Melo <peimelo@gmail.com>
2020-07-28 08:23:51 -0300

    Adiciona inicializado de opções do Devise

---
+++ config/initializers/devise.rb
@@ -0,0 +1,16 @@
+Devise.setup do |config|
+  # The e-mail address that mail will appear to be sent from
+  # If absent, mail is sent from "please-change-me-at-config-initializers-devise@example.com"
+  config.mailer_sender = 'support@myapp.com'
+
+  # ==> ORM configuration
+  # Load and configure the ORM. Supports :active_record (default) and
+  # :mongoid (bson_ext recommended) by default. Other ORMs may be
+  # available as additional gems.
+  require 'devise/orm/active_record'
+
+  # If using rails-api, you may want to tell devise to not use ActionDispatch::Flash
+  # middleware b/c rails-api does not include it.
+  # See: https://stackoverflow.com/q/19600905/806956
+  config.navigational_formats = [:json]
+end
