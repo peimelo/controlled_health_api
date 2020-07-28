7ba31e3a873c53d1562dfca4735273a0e572e187
Paulo Melo <peimelo@gmail.com>
2020-07-28 08:20:05 -0300

    Adiciona models do Devise e module confirmable

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,11 @@
 # frozen_string_literal: true
 
 class User < ActiveRecord::Base
+  extend Devise::Models
+
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+  # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable
   include DeviseTokenAuth::Concerns::User
 end
