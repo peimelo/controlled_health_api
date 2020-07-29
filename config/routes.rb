40a397ec8f0b1f352f2ac308ae8e1977076a79b1
Paulo Melo <peimelo@gmail.com>
2020-07-29 10:30:13 -0300

    Adiciona versionamento para api

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
+require 'api_constraints'
+
 Rails.application.routes.draw do
-  mount_devise_token_auth_for 'User', at: 'auth'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api, defaults: { format: :json } do
+    scope module: :v1,
+          constraints: ApiConstraints.new(version: 1, default: true) do
+      mount_devise_token_auth_for 'User', at: 'auth'
+    end
+  end
 end
