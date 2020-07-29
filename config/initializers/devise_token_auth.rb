9def1f1780bc92931fa992cc0d2cc4f7f8e8a358
Paulo Melo <peimelo@gmail.com>
2020-07-29 10:41:54 -0300

    Configura success_url após criar e confirmar user

--- config/initializers/devise_token_auth.rb
+++ config/initializers/devise_token_auth.rb
@@ -52,4 +52,6 @@ DeviseTokenAuth.setup do |config|
   # If, however, you wish to integrate with legacy Devise authentication, you can
   # do so by enabling this flag. NOTE: This feature is highly experimental!
   # config.enable_standard_devise_support = false
+
+  config.default_confirm_success_url = 'http://localhost:4200/signin'
 end
