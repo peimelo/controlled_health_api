2b51cad04e16d97d98bcba80f04c9ddc9e1537f2
Paulo Melo <peimelo@gmail.com>
2020-07-31 08:07:11 -0300

    Obriga senha atual para trocar e deixa editar name

--- config/initializers/devise_token_auth.rb
+++ config/initializers/devise_token_auth.rb
@@ -35,7 +35,7 @@ DeviseTokenAuth.setup do |config|
   # Uncomment to enforce current_password param to be checked before all
   # attribute updates. Set it to :password if you want it to be checked only if
   # password is updated.
-  # config.check_current_password_before_update = :attributes
+  config.check_current_password_before_update = :password
 
   # By default we will use callbacks for single omniauth.
   # It depends on fields like email, provider and uid.
@@ -53,5 +53,5 @@ DeviseTokenAuth.setup do |config|
   # do so by enabling this flag. NOTE: This feature is highly experimental!
   # config.enable_standard_devise_support = false
 
-  config.default_confirm_success_url = 'http://localhost:4200/signin'
+  config.default_confirm_success_url = 'http://localhost:4200/login'
 end
