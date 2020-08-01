c19b52f295d142304600b737c0cef5ee8830e8a7
Paulo Melo <peimelo@gmail.com>
2020-08-01 15:40:57 -0300

    Adiciona testes iniciais

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe 'validations' do
+    it { should validate_presence_of(:email) }
+    it { should validate_length_of(:password).is_at_least(6) }
+  end
+end
