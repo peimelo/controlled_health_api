29983c080be8e36ff46ea890ebb596c3f53f1de0
Paulo Melo <peimelo@gmail.com>
2021-01-23 17:41:10 -0300

    Filtra peso por user

--- app/models/height.rb
+++ app/models/height.rb
@@ -3,18 +3,19 @@ class Height < ApplicationRecord
 
   validates :date, :value, presence: true
 
+  scope :filter_by_user_id, ->(user_id) { where(user_id: user_id) }
   scope :ordered, lambda {
                     select(:id, :date, :value)
                       .order(date: :desc)
                   }
 
-  def self.value_by_date(date)
-    return 0 if Height.count.zero?
+  def self.value_by_date(date, user_id)
+    return 0 if Height.filter_by_user_id(user_id).count.zero?
 
-    Height.ordered.each do |height|
+    Height.filter_by_user_id(user_id).ordered.each do |height|
       return height.value if date >= height.date
     end
 
-    Height.ordered.last.value
+    Height.filter_by_user_id(user_id).ordered.last.value
   end
 end
--- app/serializers/weight_serializer.rb
+++ app/serializers/weight_serializer.rb
@@ -2,12 +2,12 @@ class WeightSerializer < ActiveModel::Serializer
   attributes :id, :date, :value, :min, :max
 
   def min
-    height = Height.value_by_date(object.date)
+    height = Height.value_by_date(object.date, object.user_id)
     object.minimum(height).to_f
   end
 
   def max
-    height = Height.value_by_date(object.date)
+    height = Height.value_by_date(object.date, object.user_id)
     object.maximum(height).to_f
   end
 end
