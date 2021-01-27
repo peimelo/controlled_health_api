c013d59d81552c47835b0b2f499f9e29008765af
Paulo Melo <peimelo@gmail.com>
2021-01-27 18:15:34 -0300

    Adiciona email em credentials

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -112,7 +112,7 @@ Rails.application.configure do
     address: 'smtp.gmail.com',
     port: 587,
     domain: 'gmail.com',
-    user_name: 'no-reply@saudecontrolada.com.br',
+    user_name: Rails.application.credentials.gmail[:user_name].to_s,
     password: Rails.application.credentials.gmail[:password],
     authentication: 'plain',
     enable_starttls_auto: true
