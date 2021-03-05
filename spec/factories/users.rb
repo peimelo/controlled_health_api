393009d5d12a34c4cad3994377e798f740350e02
Paulo Melo <peimelo@gmail.com>
2021-03-05 17:11:22 -0300

    Increases password length to 8

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,8 +2,8 @@ FactoryBot.define do
   factory :user do
     email { Faker::Internet.email }
     uid { email }
-    password { '123456' }
-    password_confirmation { '123456' }
+    password { '12345678' }
+    password_confirmation { password }
 
     factory :confirmed_user do
       confirmed_at { Time.now }
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,6 +9,6 @@ RSpec.describe User, type: :model do
 
   describe 'validations' do
     it { should validate_presence_of(:email) }
-    it { should validate_length_of(:password).is_at_least(6) }
+    it { should validate_length_of(:password).is_at_least(8) }
   end
 end
