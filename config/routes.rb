37e66b4db507e5959b19a5c03d6fc5bfc55bad2f
Paulo Melo <peimelo@gmail.com>
2021-03-11 08:40:40 -0300

    Configures auth route out of version

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ require 'api_constraints'
 
 Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
+    mount_devise_token_auth_for 'User', at: 'auth'
+
     scope module: :v1,
           constraints: ApiConstraints.new(version: 1, default: true) do
-      mount_devise_token_auth_for 'User', at: 'auth'
-
       resources :dashboards, only: [:index]
       resources :heights
       resources :results do
