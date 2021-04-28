9c288aa605a80c02f2bcd36af9e756de6030946a
Paulo Melo <peimelo@gmail.com>
2021-04-28 11:01:14 -0300

    Sortable tests

--- spec/models/height_spec.rb
+++ spec/models/height_spec.rb
@@ -2,6 +2,8 @@ require 'rails_helper'
 
 # rubocop:disable Metrics/BlockLength
 RSpec.describe Height, type: :model do
+  let(:user) { create :user }
+
   describe 'associations' do
     it { should belong_to(:user) }
   end
@@ -15,31 +17,37 @@ RSpec.describe Height, type: :model do
     it { should validate_numericality_of(:value).is_less_than_or_equal_to(250) }
   end
 
-  describe '.value_by_date' do
-    before(:all) do
-      @user = create(:user)
+  describe 'concerns' do
+    it '.sorted' do
+      expect(user.heights.order('date desc').to_sql).to eq user.heights_sorted('', '').to_sql
+      expect(user.heights.order('date desc').to_sql).to eq user.heights_sorted('v', 'a').to_sql
+      expect(user.heights.order('value desc').to_sql).to eq user.heights_sorted('value', '').to_sql
+      expect(user.heights.order('date asc').to_sql).to eq user.heights_sorted('', 'asc').to_sql
+      expect(user.heights.order('value asc').to_sql).to eq user.heights_sorted('value', 'asc').to_sql
     end
+  end
 
+  describe '.value_by_date' do
     context 'with heights' do
       before(:all) do
-        @height1 = create(:height, date: 10.years.ago, value: 100, user: @user)
-        @height2 = create(:height, date: 20.years.ago, value: 200, user: @user)
+        @user = create(:user)
+        @height_10ya = create(:height, date: 10.years.ago, value: 100, user: @user)
+        @height_20ya = create(:height, date: 20.years.ago, value: 200, user: @user)
       end
 
       it 'returns the value of an earlier date' do
-        expect(Height.value_by_date(5.years.ago, @user.id)).to eq @height1.value
-        expect(Height.value_by_date(15.years.ago, @user.id)).to eq @height2.value
+        expect(Height.value_by_date(5.years.ago, @user.id)).to eq @height_10ya.value
+        expect(Height.value_by_date(15.years.ago, @user.id)).to eq @height_20ya.value
       end
 
       it 'returns the value of the last record' do
-        expect(Height.value_by_date(25.years.ago, @user.id)).to eq @height2.value
+        expect(Height.value_by_date(25.years.ago, @user.id)).to eq @height_20ya.value
       end
     end
 
     context 'without heights' do
       it 'returns zero' do
-        Height.delete_all
-        expect(Height.value_by_date(5.years.ago, @user.id)).to eq 0
+        expect(Height.value_by_date(5.years.ago, user.id)).to eq 0
       end
     end
   end
