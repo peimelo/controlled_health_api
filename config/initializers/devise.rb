8fb86bf2fb5b92420932cc6b55af62dc165a3ef4
Paulo Melo <peimelo@gmail.com>
2021-10-14 06:23:36 -0300

    Adds mailer sender

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -3,7 +3,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = Rails.env.production? ? Rails.application.credentials.gmail[:user_name].to_s : 'devise@example.com'
+  config.mailer_sender = Rails.env.production? ? Rails.application.credentials.gmail[:mailer_sender] : 'devise@example.com'
 
   # ==> ORM configuration
   # Load and configure the ORM. Supports :active_record (default) and
