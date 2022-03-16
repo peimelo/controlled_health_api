98a7cb1ff398b797c60a385a25767fc21ed115f6
Paulo Melo <peimelo@gmail.com>
2022-03-16 11:55:51 -0300

    Adjust minimum value

--- spec/models/weight_spec.rb
+++ spec/models/weight_spec.rb
@@ -12,7 +12,7 @@ RSpec.describe Weight, type: :model do
     it { should validate_presence_of(:date) }
 
     it { should validate_presence_of(:value) }
-    it { should validate_numericality_of(:value).is_greater_than_or_equal_to(3) }
+    it { should validate_numericality_of(:value).is_greater_than_or_equal_to(1) }
     it { should validate_numericality_of(:value).is_less_than_or_equal_to(400) }
   end
 
