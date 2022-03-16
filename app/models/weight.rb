026a9814606101fc341d79c18d856a76b0f77d5d
Paulo Melo <peimelo@gmail.com>
2022-03-16 11:49:07 -0300

    Adjust minimum value

--- app/models/weight.rb
+++ app/models/weight.rb
@@ -6,7 +6,7 @@ class Weight < ApplicationRecord
   validates :date, presence: true
   validates :value, presence: true,
                     numericality: {
-                      greater_than_or_equal_to: 3,
+                      greater_than_or_equal_to: 1,
                       less_than_or_equal_to: 400
                     }
 
